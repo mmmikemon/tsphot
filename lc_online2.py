a7b87d5a3c261cb911cb4d1d2c6de0d08ff6afbb
mmmikemon <mikemon@astro.as.utexas.edu>
2015-03-15 05:41:18 -0500

    Update lc_online2.py

--- lc_online2.py
+++ lc_online2.py
@@ -115,10 +115,10 @@ def fwhm_fit(aplist,apvec,is_first_iter):
 
     if is_first_iter:
         show(block=False)
-        pause(0.0001)
+        pause(1.0)
     else:
         draw()
-        pause(0.0001)
+        pause(1.0)
     tight_layout()
 
     #psffile='psf_fit.pdf'
