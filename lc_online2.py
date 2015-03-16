4a06aaf2dde2932ed91710f6ca8be56eafb564f8
mmmikemon <mikemon@astro.as.utexas.edu>
2015-03-16 02:57:29 -0500

    Further tweaks so plots can update automatically (not "blocked")

--- lc_online2.py
+++ lc_online2.py
@@ -11,7 +11,7 @@ import matplotlib.gridspec as gridspec
 from matplotlib.widgets import CheckButtons
 from matplotlib.ticker import MaxNLocator
 
-global iline, icts
+global iline, icts, lp3a, lp3b, l1, l2, l3, l4, l5
 
 def padxlim2(ax1,xdata):
     """
@@ -27,7 +27,7 @@ def padxlim2(ax1,xdata):
     print x0, x1
     ax1.set_xlim(x0,x1)
 
-def fwhm_fit2(aplist,targs):
+def fwhm_fit2(aplist,targs,is_first_iter):   
     nlc = len(targs[:,0])
     fwhm_vec = []
     for j in np.arange(nlc):
@@ -53,23 +53,29 @@ def fwhm_fit2(aplist,targs):
     apfine = np.arange(0,max(aplist),0.1)
     psf_fit = psf((apfine),*popt)
     print 'FWHM = ',fwhm
-    fig=figure(3,figsize=(7,4.7))
-    ax1 = fig.add_subplot()
-    plot(aplist_shifted,dapvec2,'o')
-    plot(apfine,psf_fit,'-')
-    xlabel('radius (pixels)')
-    ylabel('profile')
     tstring = 'FWHM is {0:.3f} pixels\n'.format(fwhm)
     totstring = tstring 
-    x1,x2=xlim()
-    y1,y2=ylim()
-    xs=0.5
-    ys=0.8
-    xpos = x1 + xs*(x2-x1)
-    ypos = y1 + ys*(y2-y1)
-    text(xpos,ypos,totstring, horizontalalignment='center', verticalalignment='center')
+    if is_first_iter:
+        fig=figure(3,figsize=(7,4.7))
+        ax1 = fig.add_subplot()
+        lp3a, = plot(aplist_shifted,dapvec2,'o')
+        lp3b, = plot(apfine,psf_fit,'-')
+        xlabel('radius (pixels)')
+        ylabel('profile')
+        x1,x2=xlim()
+        y1,y2=ylim()
+        xs=0.5
+        ys=0.8
+        xpos = x1 + xs*(x2-x1)
+        ypos = y1 + ys*(y2-y1)
+        text(xpos,ypos,totstring, horizontalalignment='center', verticalalignment='center')
+        tight_layout()
+    else:
+        lp3a.set_xdata(aplist_shifted)
+        lp3a.set_ydata(dapvec2)
+        lp3b.set_xdata(apfine)
+        lp3b.set_ydata(psf_fit)
 
-    tight_layout()
     #show()
 
     #psffile='psf_fit.pdf'
@@ -77,54 +83,6 @@ def fwhm_fit2(aplist,targs):
     #close()
     #print 'PSF fit stored in',psffile,'\n'
     return fwhm_vec
-
-def fwhm_fit(aplist,apvec,is_first_iter):
-    f = apvec
-    ndim = len(f)
-    i = np.arange(ndim)
-    ip = i + 1
-    aplist2 = np.concatenate([ [0],aplist])
-    apvec2 = np.concatenate([ [0],apvec])
-    dapvec2 = (apvec2[ip]-apvec2[i])/(aplist2[ip]**2-aplist2[i]**2)
-    aplist_shifted = 0.5*(aplist2[ip]+aplist2[i])
-    s0 = 10.
-    s1 = dapvec2[0]
-    w = 3.
-    pinitial = np.array([ s0, s1, w ])
-    popt, pcov = sco.curve_fit(psf, aplist_shifted, dapvec2, p0=pinitial)
-    w = popt[2]
-    fwhm = 2. * np.sqrt(2.*np.log(2.)) * w
-    apfine = np.arange(0,max(aplist),0.1)
-    psf_fit = psf((apfine),*popt)
-    print 'FWHM = ',fwhm
-    fig=figure(3)
-    ax1 = fig.add_subplot()
-    plot(aplist_shifted,dapvec2,'o')
-    plot(apfine,psf_fit,'-')
-    xlabel('radius (pixels)')
-    ylabel('profile')
-    tstring = 'FWHM is {0:.3f} pixels\n'.format(fwhm)
-    totstring = tstring 
-    x1,x2=xlim()
-    y1,y2=ylim()
-    xs=0.5
-    ys=0.8
-    xpos = x1 + xs*(x2-x1)
-    ypos = y1 + ys*(y2-y1)
-    text(xpos,ypos,totstring, horizontalalignment='center', verticalalignment='center')
-
-    if is_first_iter:
-        show(block=False)
-        pause(1.0)
-    else:
-        draw()
-        pause(1.0)
-    tight_layout()
-
-    #psffile='psf_fit.pdf'
-    #savefig(psffile,transparent=True,bbox_inches='tight')
-    #close()
-    #print 'PSF fit stored in',psffile,'\n'
 
 # Gaussian functional form assumed for PSF fits
 def psf((rr),s0,s1,w):
@@ -157,11 +115,9 @@ def list_powerset2(lst):
 
 # Make a plot of light curve scatter versus aperture size
 #def applot(aplist,sigvec,apmin,cstring):
-def applot(apfile, aplist,sigvec,apmin,cstring):
+def applot(apfile, aplist,sigvec,apmin,cstring,is_first_iter):
     #apfile = 'aperture.pdf'
     #fig=figure(2)
-    fig=figure(2,figsize=(7,4.7))
-    ax1 = fig.add_subplot()
     mask = np.where(aplist > 0.)
     ap_filt = aplist[mask]
     sig_filt = sigvec[mask]
@@ -169,31 +125,33 @@ def applot(apfile, aplist,sigvec,apmin,cstring):
     ap_filt_s = ap_filt[iarg]
     sig_filt_s = sig_filt[iarg]
     #plot(ap_filt,sig_filt,'-o')
-    plot(ap_filt_s,sig_filt_s,'-o')
-    xlabel('Aperture size')
-    ylabel('Scatter')
-    tstring = 'optimal aperture is {0} pixels\n'.format(apmin)
-    cstring = 'optimal comparison star =\n' + cstring
-    totstring = tstring + '\n' + cstring
-    x1,x2=xlim()
-    y1,y2=ylim()
-    xs=0.5
-    ys=0.8
-    xpos = x1 + xs*(x2-x1)
-    ypos = y1 + ys*(y2-y1)
-    text(xpos,ypos,totstring, horizontalalignment='center', verticalalignment='center')
+    if is_first_iter:
+        fig=figure(2,figsize=(7,4.7))
+        ax1 = fig.add_subplot()
+        la, = plot(ap_filt_s,sig_filt_s,'-o')
+        xlabel('Aperture size')
+        ylabel('Scatter')
+        tstring = 'optimal aperture is {0} pixels\n'.format(apmin)
+        cstring = 'optimal comparison star =\n' + cstring
+        totstring = tstring + '\n' + cstring
+        x1,x2=xlim()
+        y1,y2=ylim()
+        xs=0.5
+        ys=0.8
+        xpos = x1 + xs*(x2-x1)
+        ypos = y1 + ys*(y2-y1)
+        text(xpos,ypos,totstring, horizontalalignment='center', verticalalignment='center')
 
-    tight_layout()
-    #show()
+        tight_layout()
+    else:
+        la.set_xdata( ap_filt_s)
+        la.set_ydata(sig_filt_s)
 
-    #savefig(apfile,transparent=True,bbox_inches='tight')
-    #close()
-    #print 'Aperture optimization stored in',apfile
     
 # Make a plot of the optimal light curve file
 def lcplot(flc_pdf, time,target,comp,sky,fwhm_vec,cstring,is_first_iter):
 #def lcplot(time,target,comp,sky,fwhm_vec,cstring):
-    global iline, icts
+    global iline, icts, l1, l2, l3, l4, l5
     ratio = target/comp
     ratio_norm = ratio/np.mean(ratio) - 1.
     #scipy.convolve(y, ones(N)/N)
@@ -219,104 +177,118 @@ def lcplot(flc_pdf, time,target,comp,sky,fwhm_vec,cstring,is_first_iter):
     pow = pow * 4/ndata
     amp = np.sqrt(pow)
 
-    fig=figure(1,figsize=(11, 11))
-    gs1 = gridspec.GridSpec(6, 1,hspace=0.0)
-    ax1 = fig.add_subplot(gs1[0])
-    l1, = ax1.plot(time,ratio_norm,'o')
-    #ax1.set_xlim([-500,15000])
-    ax1.yaxis.set_major_locator( MaxNLocator(nbins = 6) )
-    ylabel(r'$\delta I/I$',size='x-large')
-    leg1=ax1.legend(['target'],'best',fancybox=True,shadow=False,markerscale=0.0,frameon=False)
-    setp(ax1.get_xticklabels(), visible=False)
-    #padxlim2(ax1,time)
-
-    ax2 = fig.add_subplot(gs1[1],sharex=ax1)
-    l2, = plot(time_smooth[0:-nfilt],ratio_norm_smooth[:-nfilt],'o')
-    ax2.yaxis.set_major_locator( MaxNLocator(nbins = 6, prune = 'upper') )
-    ylabel(r'$\delta I/I$',size='x-large')
-    leg=ax2.legend(['target smoothed'],'best',fancybox=True,shadow=False,markerscale=0.0,frameon=False)
-    setp(ax2.get_xticklabels(), visible=False)
-
-    ax3 = fig.add_subplot(gs1[2],sharex=ax1)
-    l3, = plot(time,comp_norm,'o')
-    ax3.yaxis.set_major_locator( MaxNLocator(nbins = 6, prune = 'upper') )
-    ylabel(r'$\delta I/I$',size='x-large')
-    comstring = 'comparison\n' + '= ' + cstring
-    leg=ax3.legend([comstring],'best',fancybox=True,shadow=False,markerscale=0.0,frameon=False)
-    setp(ax3.get_xticklabels(), visible=False)
-
-    ax4 = fig.add_subplot(gs1[3],sharex=ax1)
-    pl1 =ax4.plot(time,sky_norm,'o',label = 'Sky')
-    l4, = pl1
-    ax4.yaxis.set_major_locator( MaxNLocator(nbins = 5, prune = 'upper') )
-    ylabel('Sky',size='large')
-    xlabel('time (sec)',size='large')
-    fwhm_string = 'FWHM={0:.3f}'.format(fwhm_vec[-1])
-    ax6 = ax4.twinx()
-    pl2 = ax6.plot(time, fwhm_vec, 'ro',label = fwhm_string)
-    l5, = pl2
-    pls = pl1 + pl2
-    ax6.set_ylabel('FWHM', color='r')
-    for tl in ax6.get_yticklabels():
-        tl.set_color('r')
-
-    labs = [l.get_label() for l in pls]
-    leg=ax4.legend(pls, labs, 'best', frameon = False, ncol=2, handletextpad=0.0, columnspacing=0.0)
-
-    gs2 = gridspec.GridSpec(4, 1,hspace=0.1)
-    ax5 = fig.add_subplot(gs2[3])
-
-    freqs = farray * 1.e+6
-    plot(freqs,amp)
-    xmax=max(freqs)
-    xlim(0.,xmax)
-    xlabel(r'Frequency ($\mu$Hz)',size='large')
-    ylabel('Amplitude',size='large')
-    leg=ax5.legend(['FT'],'best',fancybox=True,shadow=False,handlelength=0.0)
-    leg.draw_frame(False)
-
-    rax = axes([0.46, 0.905, 0.11, 0.07])
-    lin = ' lines'
-    cts = ' counts'
-    check = CheckButtons(rax, (lin,' counts'), (False, False))
-    iline = 1
-    icts = 1
-    lset = [l1, l2, l3, l4, l5]
-    def stylefunc(label):
-        global iline, icts
-        if label == lin: 
-            if iline == 1:
-                for ll in lset:
-                    ll.set_linestyle('-')
-                    ll.set_marker('None')
-            else:
-                for ll in lset:
-                    ll.set_linestyle('o')
-                    ll.set_marker('o')
-            iline = -1 * iline
-        if label == cts: 
-            if icts == 1:
-                l3.set_ydata(comp)
-                ax3.set_ylabel('Comp',size='large')
-                l4.set_ydata(sky)
-            else:
-                l3.set_ydata(comp_norm)
-                ax3.set_ylabel(r'$\delta I/I$',size='x-large')
-                l4.set_ydata(sky_norm)
-            ax3.relim()
-            ax3.autoscale_view()
-            ax4.relim()
-            ax4.autoscale_view()
-            icts = -1 * icts
-        draw()
-
-    check.on_clicked(stylefunc)
-
-    #tight_layout()
     if is_first_iter:
+        fig=figure(1,figsize=(11, 11))
+        gs1 = gridspec.GridSpec(6, 1,hspace=0.0)
+        ax1 = fig.add_subplot(gs1[0])
+        l1, = ax1.plot(time,ratio_norm,'o')
+        #ax1.set_xlim([-500,15000])
+        ax1.yaxis.set_major_locator( MaxNLocator(nbins = 6) )
+        ylabel(r'$\delta I/I$',size='x-large')
+        leg1=ax1.legend(['target'],'best',fancybox=True,shadow=False,markerscale=0.0,frameon=False)
+        setp(ax1.get_xticklabels(), visible=False)
+        #padxlim2(ax1,time)
+
+        ax2 = fig.add_subplot(gs1[1],sharex=ax1)
+        l2, = plot(time_smooth[0:-nfilt],ratio_norm_smooth[:-nfilt],'o')
+        ax2.yaxis.set_major_locator( MaxNLocator(nbins = 6, prune = 'upper') )
+        ylabel(r'$\delta I/I$',size='x-large')
+        leg=ax2.legend(['target smoothed'],'best',fancybox=True,shadow=False,markerscale=0.0,frameon=False)
+        setp(ax2.get_xticklabels(), visible=False)
+
+        ax3 = fig.add_subplot(gs1[2],sharex=ax1)
+        l3, = plot(time,comp_norm,'o')
+        ax3.yaxis.set_major_locator( MaxNLocator(nbins = 6, prune = 'upper') )
+        ylabel(r'$\delta I/I$',size='x-large')
+        comstring = 'comparison\n' + '= ' + cstring
+        leg=ax3.legend([comstring],'best',fancybox=True,shadow=False,markerscale=0.0,frameon=False)
+        setp(ax3.get_xticklabels(), visible=False)
+
+        ax4 = fig.add_subplot(gs1[3],sharex=ax1)
+        pl1 =ax4.plot(time,sky_norm,'o',label = 'Sky')
+        l4, = pl1
+        ax4.yaxis.set_major_locator( MaxNLocator(nbins = 5, prune = 'upper') )
+        ylabel('Sky',size='large')
+        xlabel('time (sec)',size='large')
+        fwhm_string = 'FWHM={0:.3f}'.format(fwhm_vec[-1])
+        ax6 = ax4.twinx()
+        pl2 = ax6.plot(time, fwhm_vec, 'ro',label = fwhm_string)
+        l5, = pl2
+        pls = pl1 + pl2
+        ax6.set_ylabel('FWHM', color='r')
+        for tl in ax6.get_yticklabels():
+            tl.set_color('r')
+
+        labs = [l.get_label() for l in pls]
+        leg=ax4.legend(pls, labs, 'best', frameon = False, ncol=2, handletextpad=0.0, columnspacing=0.0)
+
+        gs2 = gridspec.GridSpec(4, 1,hspace=0.1)
+        ax5 = fig.add_subplot(gs2[3])
+
+        freqs = farray * 1.e+6
+        plot(freqs,amp)
+        xmax=max(freqs)
+        xlim(0.,xmax)
+        xlabel(r'Frequency ($\mu$Hz)',size='large')
+        ylabel('Amplitude',size='large')
+        leg=ax5.legend(['FT'],'best',fancybox=True,shadow=False,handlelength=0.0)
+        leg.draw_frame(False)
+
+        rax = axes([0.46, 0.905, 0.11, 0.07])
+        lin = ' lines'
+        cts = ' counts'
+        check = CheckButtons(rax, (lin,' counts'), (False, False))
+        iline = 1
+        icts = 1
+        lset = [l1, l2, l3, l4, l5]
+        def stylefunc(label):
+            global iline, icts
+            if label == lin: 
+                if iline == 1:
+                    for ll in lset:
+                        ll.set_linestyle('-')
+                        ll.set_marker('None')
+                else:
+                    for ll in lset:
+                        ll.set_linestyle('o')
+                        ll.set_marker('o')
+                iline = -1 * iline
+            if label == cts: 
+                if icts == 1:
+                    l3.set_ydata(comp)
+                    ax3.set_ylabel('Comp',size='large')
+                    l4.set_ydata(sky)
+                else:
+                    l3.set_ydata(comp_norm)
+                    ax3.set_ylabel(r'$\delta I/I$',size='x-large')
+                    l4.set_ydata(sky_norm)
+                ax3.relim()
+                ax3.autoscale_view()
+                ax4.relim()
+                ax4.autoscale_view()
+                icts = -1 * icts
+            draw()
+
         show(block=False)
+        check.on_clicked(stylefunc)
     else:
-        draw()
+        #l1, = ax1.plot(time,ratio_norm,'o')
+        l1.set_xdata(time)
+        l1.set_ydata(ratio_norm)
+        #l2, = plot(time_smooth[0:-nfilt],ratio_norm_smooth[:-nfilt],'o')
+        l2.set_xdata(time_smooth[0:-nfilt])
+        l2.set_ydata(ratio_norm_smooth[:-nfilt])
+        #l3, = plot(time,comp_norm,'o')
+        l3.set_xdata(time)
+        l3.set_ydata(comp_norm)
+        #pl1 =ax4.plot(time,sky_norm,'o',label = 'Sky')
+        #l4, = pl1
+        l4.set_xdata(time)
+        l4.set_ydata(sky_norm)
+        #pl2 = ax6.plot(time, fwhm_vec, 'ro',label = fwhm_string)
+        #l5, = pl2
+        l5.set_xdata(time)
+        l5.set_ydata(fwhm_vec)
 
     #filebase = 'lc.pdf'
     #savefig(filebase,transparent=True,bbox_inches='tight')
@@ -452,7 +424,7 @@ def main(args,is_first_iter):
 
     # Make plot of scatter versus aperture size
     sigvec = ysigarr[:,ncmin]
-    applot(args.fap_pdf, aplist,sigvec,apmin,cstring)
+    applot(args.fap_pdf, aplist,sigvec,apmin,cstring,is_first_iter)   
 
     time = 86400.*(jd-jd[0])
     target = targs[:,iapmin]
@@ -467,9 +439,9 @@ def main(args,is_first_iter):
     # Do FWHM fits for star at all points of light curve.
     # Choose comparison or target star based on lower level of scatter.
     if comp_ysig <= sigmin:
-        fwhm_vec = fwhm_fit2(aplist,comps[:,:,ncmin])   # FWHM of composite comparison star
+        fwhm_vec = fwhm_fit2(aplist,comps[:,:,ncmin],is_first_iter)   # FWHM of composite comparison star
     else:
-        fwhm_vec = fwhm_fit2(aplist,targs)   # FWHM of target star
+        fwhm_vec = fwhm_fit2(aplist,targs,is_first_iter)   # FWHM of target star
 
     # Make online plot of lightcurves, sky, and the FT
     lcplot(args.flc_pdf, time,target,compstar,sky0,fwhm_vec,cstring,is_first_iter)
